5b9155af58e633f4a09711004f17328eb2c2aade
Matheussgsilva <matheussgsilva@hotmail.com>
2022-07-20 22:55:55 -0300

    Install react navigation and create the app routes file

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,5 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Home } from '../screens/Home';
+import { Details } from '../screens/Details';
+import { Register } from '../screens/Register';
+import { SignIn } from '../screens/Signin';
