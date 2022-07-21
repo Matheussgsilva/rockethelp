579e19db57abf6e7df54725834a753e2a739f2c2
Matheussgsilva <matheussgsilva@hotmail.com>
2022-07-20 21:51:59 -0300

    Create the Register screen and import the Header component

---
+++ src/screens/Register.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { VStack } from 'native-base';
+import { Header } from '../components/Header';
+
+export function Register() {
+  return (
+    <VStack flex={1} p={6} bg='gray.600' >
+        <Header title='Nova solicitação' />
+    </VStack>
+  );
+}
