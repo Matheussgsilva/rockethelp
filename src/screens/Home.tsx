252f32210ca1019ab5878af755573dbcce3daf49
Matheussgsilva <matheussgsilva@hotmail.com>
2022-07-21 18:38:04 -0300

    Create navigation function to Details screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -22,6 +22,10 @@ export function Home() {
         navigation.navigate('new')
     }
 
+    function handleOpenDetails(orderId: string) {
+        navigation.navigate('details', { orderId })
+    }
+
   return (
     <VStack flex={1} pb={6} bg='gray.700' >
         <HStack
@@ -64,7 +68,7 @@ export function Home() {
             <FlatList 
                 data={orders}
                 keyExtractor={item => item.id}
-                renderItem={({ item }) => <Order data={item}/>}
+                renderItem={({ item }) => <Order data={item} onPress={() => handleOpenDetails(item.id)}/>}
                 showsVerticalScrollIndicator={false}
                 contentContainerStyle={{ paddingBottom: 100 }}
                 ListEmptyComponent={() => (
