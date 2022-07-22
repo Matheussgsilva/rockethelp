e19b04ccb079148e47c6beade8d2be7955bde414
Matheussgsilva <matheussgsilva@hotmail.com>
2022-07-22 20:47:40 -0300

    Create Logout function

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { Alert } from 'react-native';
+import auth from '@react-native-firebase/auth'
 import { useNavigation } from '@react-navigation/native';
 import { HStack, IconButton, VStack, useTheme, Heading, Text, FlatList, Center } from 'native-base';
 import { Filter } from '../components/Filter';
@@ -26,6 +28,14 @@ export function Home() {
         navigation.navigate('details', { orderId })
     }
 
+    function handleLogout() {
+        auth().signOut()
+        .catch(error =>{
+            console.log(error)
+            return Alert.alert('Sair', 'Não foi possível desconectar.')
+        })
+    }
+
   return (
     <VStack flex={1} pb={6} bg='gray.700' >
         <HStack
@@ -40,6 +50,7 @@ export function Home() {
             <Logo />
             <IconButton 
                 icon={<SignOut color={colors.gray[300]} />}
+                onPress={handleLogout}
             />
         </HStack>
         <VStack flex={1} px={6}>
