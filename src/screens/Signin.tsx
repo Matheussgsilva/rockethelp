6c6983ed1975ce286d1dd83a487030331a6fa014
Matheussgsilva <matheussgsilva@hotmail.com>
2022-07-22 20:33:58 -0300

    Create the auth function and add loading in button

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import auth from '@react-native-firebase/auth';
+import { Alert } from "react-native";
 import { VStack, Heading, Icon, useTheme } from "native-base";
 import { Envelope, Key } from 'phosphor-react-native';
 import Logo from '../assets/logo_primary.svg';
@@ -9,7 +11,35 @@ import { Button } from "../components/Button";
 export function SignIn() {
     const [emailField, setEmailField] = useState('')
     const [passwordField, setPasswordField] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
     const { colors } = useTheme()
+
+    function handleSignIn() {
+        if(!emailField || !passwordField) {
+            return Alert.alert('Entrar', 'Informe e-mail e senha.')
+        }
+        setIsLoading(true)
+
+        auth().signInWithEmailAndPassword(emailField, passwordField)
+        .catch((error) => {
+            console.log(error)
+            setIsLoading(false)
+
+            if(error.code === 'auth/invalid-email') {
+                return Alert.alert('Entrar', 'E-mail inválido.')
+            }
+
+            if(error.code === 'auth/user-not-found') {
+                return Alert.alert('Entrar', 'E-mail ou senha inválidos.')
+            }
+
+            if(error.code === 'auth/wrong-password') {
+                return Alert.alert('Entrar', 'E-mail ou senha inválidos.')
+            }
+
+            return Alert.alert('Entrar', 'Não foi possível acessar')
+        })
+    }
 
     return (
         <VStack flex={1} alignItems='center' bg='gray.600' px={8} pt={24}>
@@ -33,6 +63,8 @@ export function SignIn() {
             <Button 
                 title="Entrar"
                 w='full'
+                onPress={handleSignIn}
+                isLoading={isLoading}
             />
         </VStack>
     )
